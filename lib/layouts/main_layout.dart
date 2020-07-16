821b57206bdb18874cb159ff832c3f06eca88dd1
Ilya Kozlov <ilya@kozlov.dev>
2020-07-16 20:33:09 +0600

    added main layout with bottom navigation bar

---
+++ lib/layouts/main_layout.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import '../screens/products_overview_screen.dart';
+
+class MainLayout extends StatefulWidget {
+  @override
+  _MainLayoutState createState() => _MainLayoutState();
+}
+
+class _MainLayoutState extends State<MainLayout> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = <Widget>[
+    ProducrsOverviewScreen(),
+    Center(
+      child: Text(
+        'Index 1: Search page',
+      ),
+    )
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('eShop'),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text('Home')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search), title: Text('Search'))
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          unselectedItemColor: Colors.black54,
+          selectedItemColor: Colors.black,
+        ),
+        body: _screens[_selectedIndex]);
+  }
+}
