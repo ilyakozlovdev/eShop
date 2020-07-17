79e20f76a228ba973b191fa73426e923d3747b52
Ilya Kozlov <ilya@kozlov.dev>
2020-07-17 21:55:17 +0600

    added titles to items in image slider

--- lib/widgets/image_carousel.dart
+++ lib/widgets/image_carousel.dart
@@ -13,10 +13,22 @@ class _ImageCarousel extends State<ImageCarousel> {
 
   @override
   Widget build(BuildContext context) {
-    final List imgList = [
-      'https://icdn2.digitaltrends.com/image/digitaltrends/apple-macbook-pro-16-ry-11.jpg',
-      'https://i.gadgets360cdn.com/large/dell_xps_15_9500_image_notebookcheck_dell_france_1588254699768.jpg',
-      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/computer-monitors-index-1573157429.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*'
+    final List<Map<String, Object>> imgList = [
+      {
+        'title': 'New Apple MacBook Pro 16',
+        'imageUrl':
+            'https://icdn2.digitaltrends.com/image/digitaltrends/apple-macbook-pro-16-ry-11.jpg'
+      },
+      {
+        'title': 'Dell XPS 15 - perfect working machine',
+        'imageUrl':
+            'https://i.gadgets360cdn.com/large/dell_xps_15_9500_image_notebookcheck_dell_france_1588254699768.jpg'
+      },
+      {
+        'title': 'Why 60 Hz is not enough?',
+        'imageUrl':
+            'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/computer-monitors-index-1573157429.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*'
+      },
     ];
 
     return Container(
@@ -28,13 +40,42 @@ class _ImageCarousel extends State<ImageCarousel> {
       ]),
       child: CarouselSlider(
         items: imgList
-            .map((imageUrl) => Container(
-                  child: Image.network(
-                    imageUrl,
-                    fit: BoxFit.cover,
-                    width: double.infinity,
+            .map((note) => Stack(children: [
+                  Container(
+                    child: Image.network(
+                      note['imageUrl'],
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
                   ),
-                ))
+                  Positioned(
+                    bottom: 0.0,
+                    left: 0.0,
+                    right: 0.0,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromARGB(200, 0, 0, 0),
+                            Color.fromARGB(0, 0, 0, 0)
+                          ],
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                        ),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      child: Text(
+                        note['title'],
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  )
+                ]))
             .toList(),
         options: CarouselOptions(
             autoPlay: true,
