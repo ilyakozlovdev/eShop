623d15ed6092c008c2a2e6d594ccdcbc1e5321c3
Ilya Kozlov <ilya@kozlov.dev>
2020-07-17 19:45:34 +0600

    refactor: removed unused variables

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import '../widgets/image_carousel.dart';
 import '../widgets/horizontal_product_list.dart';
 
 class ProducrsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
     return ListView(
       children: [
         Container(
