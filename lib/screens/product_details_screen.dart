248c1e28ac0fd75d8db39b6692fd45fc390da10c
Ilya Kozlov <ilya@kozlov.dev>
2020-07-25 19:38:22 +0600

    initialized product details screen

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,20 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/products_provider.dart';
-import '../providers/product.dart';
+import '../providers/product_provider.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   static String routeName = '/product-details';
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
-    final Product product =
+    final ProductProvider product =
         Provider.of<ProductsProvider>(context, listen: false)
             .findById(productId);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) => ListView(
+          children: [
+            Container(
+              height: constraints.maxHeight,
+              color: Colors.black12,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        height: constraints.maxHeight * 0.3,
+                        width: double.infinity,
+                        decoration: BoxDecoration(boxShadow: [
+                          BoxShadow(
+                              blurRadius: 6,
+                              color: Colors.black.withOpacity(0.2))
+                        ]),
+                        child: Image.network(
+                          product.imageUrl,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Text('Description'),
+                      Text(product.description),
+                      Text('Price'),
+                      Text('\$${product.price}'),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: RaisedButton(
+                          child: Text('Add to cart'),
+                          color: Colors.amber,
+                          onPressed: () => null,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: RaisedButton(
+                          child: Text(
+                            'Mark as favorite',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          color: Colors.pink,
+                          onPressed: () => null,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
