45140fe6966aecde05c3e8311510e378561546e8
Ilya Kozlov <ilya@kozlov.dev>
2020-07-26 23:45:49 +0600

    added in product provider remove from cart fn

--- lib/providers/product_provider.dart
+++ lib/providers/product_provider.dart
@@ -27,4 +27,9 @@ class ProductProvider with ChangeNotifier {
     isInCart = !isInCart;
     notifyListeners();
   }
+
+  void removeFromCart() {
+    isInCart = false;
+    notifyListeners();
+  }
 }
